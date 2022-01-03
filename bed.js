3b504052e77ba68eaac5fa7b6245e09ca61b3063
Eshaan <78539313+Eshaan-K-A@users.noreply.github.com>
2022-01-03 17:42:12 +0530

    Add files via upload

---
+++ bed.js
@@ -0,0 +1,32 @@
+var status1 = "";
+var img = "";
+function preload(){
+    img = loadImage("Bed.png")
+}
+function setup(){
+    canvas = createCanvas(700, 500);
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("statusSpan").innerHTML = "Detecting Object(s)"
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status1 = true;
+    objectDetector.detect(img, gotResults);
+}
+function gotResults(error, results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+    }
+}
+function draw(){
+    image(img, 0, 0, 700, 500);
+}
+
+function back(){
+    window.location = "index.html";
+}
